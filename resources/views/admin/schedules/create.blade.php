d3278b558007f639b3df3222f3cbd95978272ba0
OliBearr <olivertm2104@gmail.com>
2025-12-08 00:31:06 +0800

    Changes in color scheme

--- resources/views/admin/schedules/create.blade.php
+++ resources/views/admin/schedules/create.blade.php
@@ -42,7 +42,7 @@
                             </div>
                         </div>
 
-                        <button class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+                        <button class="bg-blue hover:bg-blue text-black font-bold py-2 px-4 rounded">
                             Save Schedule
                         </button>
                     </form>
