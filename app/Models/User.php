b32f6e6eb0e8e42b35ad252d097561156c5db2e7
OliBearr <olivertm2104@gmail.com>
2025-12-14 20:52:04 +0800

    Add manual Resend verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,23 @@ class User extends Authenticatable implements MustVerifyEmail
     }
     public function sendEmailVerificationNotification()
     {
-        $this->notify(new VerifyEmail);
+        // 1. Generate the secure verification link
+        $url = \Illuminate\Support\Facades\URL::temporarySignedRoute(
+            'verification.verify',
+            \Carbon\Carbon::now()->addMinutes(60),
+            ['id' => $this->getKey(), 'hash' => sha1($this->getEmailForVerification())]
+        );
+
+        // 2. Send the email directly to Resend's API
+        \Illuminate\Support\Facades\Http::withHeaders([
+            'Authorization' => 'Bearer ' . env('RESEND_KEY'),
+            'Content-Type' => 'application/json'
+        ])->post('https://api.resend.com/emails', [
+            'from' => 'onboarding@resend.dev', // You must use this until you verify a domain
+            'to' => $this->email,
+            'subject' => 'Verify Your Email Address',
+            'html' => '<p>Click the link below to verify your email address:</p>
+                       <a href="'.$url.'">'.$url.'</a>'
+        ]);
     }
 }
